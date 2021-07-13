e4bd342a82c731fa36b2d63fa96d93b435974132
SolChanP <qkrthfcks1@naver.com>
2021-07-13 19:44:21 +0900

    명령 프롬프트

---
+++ 1032.py
@@ -0,0 +1,12 @@
+# 명령 프롬프트
+
+n = int(input())
+a = list(input())
+
+for i in range(n - 1):
+    b = list(input())
+    for j in range(len(a)):
+        if(a[j] != b[j]):
+            a[j] = '?'
+
+print(''.join(a))
