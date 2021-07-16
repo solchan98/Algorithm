c0ff7388298caa2eb0986448e79d5ec1dd43999b
SolChanP <qkrthfcks1@naver.com>
2021-07-16 21:46:57 +0900

    07/16#2

---
+++ 10809.py
@@ -0,0 +1,9 @@
+# 알파벳 찾기
+
+s = input()
+abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+result = ""
+for i in abc:
+    result += str(s.find(i)) + " "
+
+print(result)
