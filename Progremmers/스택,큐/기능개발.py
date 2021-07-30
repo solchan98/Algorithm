17a81cb0219ce873ec64da68e27eb7e671a247c6
SolChanP <qkrthfcks1@naver.com>
2021-07-30 16:50:39 +0900

    07/30

---
+++ Progremmers/스택,큐/기능개발.py
@@ -0,0 +1,21 @@
+# 기능개발, Lever2
+
+answer = [93, 30, 55]
+s = [1, 30, 5]
+def solution(progresses, speeds):
+    answer = []
+    time = 0
+    count = 0
+    while len(progresses)> 0:
+        if (progresses[0] + time*speeds[0]) >= 100: 
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
+print(solution(answer, s))
