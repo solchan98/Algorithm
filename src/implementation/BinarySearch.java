ee894b071519c136dda70378d13563cccadceb28
solchan98 <dev.pinetree@gmail.com>
2021-10-18 14:07:25 +0900

    add: BinarySearch 비재귀 구현

--- src/implementation/BinarySearch.java
+++ src/implementation/BinarySearch.java
@@ -1,12 +1,26 @@
 package implementation;
 
 public class BinarySearch {
+
     public int recursionBinarySearch(int arr[], int target, int low, int high) {
         if(low <= high) {
             int middle = (low + high) / 2;
             if (target == arr[middle]) return middle;
             else if (target < arr[middle]) return recursionBinarySearch(arr, target, low, middle - 1);
             else if (target > arr[middle]) return recursionBinarySearch(arr, target, middle + 1, high);
+        }
+        return -1;
+    }
+
+    public int binarySearch(int arr[], int target, int size) {
+        int low = 0;
+        int high = size - 1;
+
+        while(low <= high) {
+            int middle = (low + high) / 2;
+            if(arr[middle] == target) return middle;
+            else if(arr[middle] > target) high = middle - 1;
+            else if(arr[middle] < target) low = middle + 1;
         }
         return -1;
     }
--- src/implementation/Main.java
+++ src/implementation/Main.java
@@ -4,14 +4,23 @@ import java.util.Arrays;
 
 public class Main {
 
+//    // MergeSort
+//    public static void main(String[] args) {
+////        int arr[] = {5, 2 , 3, 8, 12, 10, 1};
+//        int arr[] = {4, 3, 8, 7, 6, 5};
+//        System.out.println("합병정렬 전: ");
+//        Arrays.stream(arr).forEach(x -> System.out.printf("%d " ,x));
+//        MergeSort mergeSort = new MergeSort();
+//        mergeSort.recursionMergeSort(arr, 0, arr.length -1);
+//        System.out.println("\n합병정렬 후: ");
+//        Arrays.stream(arr).forEach(x -> System.out.printf("%d " ,x));
+//    }
+
+    // BinarySearch
     public static void main(String[] args) {
-//        int arr[] = {5, 2 , 3, 8, 12, 10, 1};
-        int arr[] = {4, 3, 8, 7, 6, 5};
-        System.out.println("합병정렬 전: ");
-        Arrays.stream(arr).forEach(x -> System.out.printf("%d " ,x));
-        MergeSort mergeSort = new MergeSort();
-        mergeSort.recursionMergeSort(arr, 0, arr.length -1);
-        System.out.println("\n합병정렬 후: ");
-        Arrays.stream(arr).forEach(x -> System.out.printf("%d " ,x));
+        int arr[] = {1, 2, 3, 5, 6, 9, 10};
+        BinarySearch binarySearch = new BinarySearch();
+        System.out.println("(재귀)인덱스 번호: " + binarySearch.recursionBinarySearch(arr, 5,0,arr.length - 1));
+        System.out.println("(비재귀)인덱스 번호: " + binarySearch.binarySearch(arr, 5, arr.length));
     }
 }
