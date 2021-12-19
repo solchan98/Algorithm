17996a32111604c3591e76833205e2d311a632a8
solchan98 <dev.sol0427@gmail.com>
2021-12-19 23:42:20 +0900

    feat: QuickSort 단어 첫 글자(ASCII) 기준 정렬 구현(대문자 > 소문자)

--- src/implementation/QuickSort.java
+++ src/implementation/QuickSort.java
@@ -2,13 +2,37 @@ package implementation;
 
 public class QuickSort {
 
-    private void swap(int arr[], int first, int second) {
+    /* QuickSort for Integer */
+    public void quickSort(int[] arr, int low, int high) {
+        if(low < high) {
+            int s = partition(arr, low, high);
+            quickSort(arr, low, s - 1);
+            quickSort(arr, s + 1, high);
+        }
+    }
+    
+    /* QuickSort for Word */
+    public void quickSort(String[] arr, int low, int high) {
+        if(low < high) {
+            int s = partition(arr, low, high);
+            quickSort(arr, low, s - 1);
+            quickSort(arr, s + 1, high);
+        }
+    }
+
+    private void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
         arr[second] = temp;
     }
 
-    private int partition(int arr[], int low, int high) {
+    private void swap(String[] arr, int first, int second) {
+        String temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private int partition(int[] arr, int low, int high) {
         int left = low + 1;
         int right = high;
 
@@ -24,11 +48,19 @@ public class QuickSort {
         return right;
     }
 
-    public void quickSort(int arr[], int low, int high) {
-        if(low < high) {
-            int s = partition(arr, low, high);
-            quickSort(arr, low, s - 1);
-            quickSort(arr, s + 1, high);
+    private int partition(String[] arr, int low, int high) {
+        int left = low + 1;
+        int right = high;
+        while(left <= right) {
+            if(arr[left].charAt(0) <= arr[low].charAt(0)) {
+                left++;
+            } else if(arr[right].charAt(0) > arr[low].charAt(0)) {
+                right--;
+            } else {
+                swap(arr, left++, right--);
+            }
         }
+        swap(arr, low, right);
+        return right;
     }
 }
