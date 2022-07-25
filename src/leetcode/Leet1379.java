df23197bc77557f783c84d5c32653ca5c28c02b3
solchan98 <dev.sol0427@gmail.com>
2022-07-25 15:12:27 +0900

    feat: <leetcode> find a corresponding node of a binary tree in a clone of that tree

---
+++ src/leetcode/Leet1379.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+ }
+
+public class Leet1379 {
+    TreeNode result;
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (result != null) return result;
+        if (original != null) {
+            getTargetCopy(original.left, cloned.left, target);
+            if (original == target) {
+                result = cloned;
+            }
+            getTargetCopy(original.right, cloned.right, target);
+        }
+        return result;
+    }
+}
