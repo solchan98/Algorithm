1185e29414b64a50b867dc46646a8cc95c300860
solchan98 <dev.sol0427@gmail.com>
2022-08-06 17:32:31 +0900

    refactir: <leetcode> Find a Corresponding Node of a Binary Tree in a Clone of That Tree (불필요 접근 제거하여 평균 메모리 100Mb -> 40 Mb로 최적화)

--- src/main/java/leetcode/Leet1379.java
+++ src/main/java/leetcode/Leet1379.java
@@ -1,16 +1,19 @@
 package leetcode;
 
+import java.util.Objects;
+
 public class Leet1379 {
     TreeNode result;
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
         if (result != null) return result;
-        if (original != null) {
-            getTargetCopy(original.left, cloned.left, target);
+        if (!Objects.isNull(original)) {
             if (original == target) {
                 result = cloned;
+            } else {
+                getTargetCopy(original.left, cloned.left, target);
+                getTargetCopy(original.right, cloned.right, target);
             }
-            getTargetCopy(original.right, cloned.right, target);
-        }
+        };
         return result;
     }
 }
