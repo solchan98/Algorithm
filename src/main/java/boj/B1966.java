74a5d1fbf03947810f19f867ac7c1fba1ee9d15d
solchan98 <dev.sol0427@gmail.com>
2022-10-18 14:46:37 +0900

    feat: <boj> 프린터 큐 (1966)

--- src/main/java/boj/B1966.java
+++ src/main/java/boj/B1966.java
@@ -3,40 +3,77 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Objects;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class B1966 {
 
+    static int n;
+    static BufferedReader br;
+
+    static List<Task> taskList;
+    static int target;
+
+    static class Task {
+        int idx;
+        int weight;
+
+        public Task(int idx, int weight) {
+            this.idx = idx;
+            this.weight = weight;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+        init();
+        while (n-- > 0) run();
+    }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    private static void run() throws IOException {
+        subInit();
+        int i = 0;
 
-        int caseCnt = Integer.parseInt(st.nextToken());
-
-        for(int i = 0; i < caseCnt; i++) {
-            String[] input =  br.readLine().split(" ");
-            int te = te(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
-            System.out.println(te);
+        while (!taskList.isEmpty()) {
+            Task task = taskList.remove(0);
+            boolean isPrintable = check(task);
+            if (!isPrintable) {
+                taskList.add(task);
+            } else {
+                i++;
+                if (task.idx == target + 1) {
+                    System.out.println(i);
+                    break;
+                }
+            }
         }
     }
 
-    private static int te(int cnt, int target) {
-        int result = 0;
-        Deque<Integer> deque = new LinkedList<>();
-        for (int i = 0; i < cnt; i++) deque.push(i);
-        Integer p = deque.poll();
-        if (Objects.isNull(p)) return 0;
-        while (p != target) {
-            result += 1;
-            p = deque.poll();
-            if (Objects.isNull(p)) return 0;
-        }
+    private static boolean check(Task task) {
+        int weight = task.weight;
 
-        return result;
+        for (Task t: taskList) {
+            if (t.weight > weight) {
+                return false;
+            }
+        }
+        return  true;
     }
 
+    private static void subInit() throws IOException {
+        String[] strings = br.readLine().split(" ");
+        int cnt = Integer.parseInt(strings[0]);
+        target = Integer.parseInt(strings[1]);
+        taskList = new ArrayList<>(cnt);
+
+        String[] weights = br.readLine().split(" ");
+
+        for (int idx = 1; idx <= cnt; idx++) {
+            int weight = Integer.parseInt(weights[idx - 1]);
+            taskList.add(new Task(idx, weight));
+        }
+    }
+
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+    }
 }
