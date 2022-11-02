b2caacc6d1792078109b7106cf0d9f69d1d7555c
solchan98 <dev.sol0427@gmail.com>
2022-11-02 18:36:33 +0900

    feat: <boj> 에디터<시간초과 해결> (1406)

--- src/main/java/boj/B1406.java
+++ src/main/java/boj/B1406.java
@@ -3,55 +3,71 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class B1406 {
 
     static int commandCnt;
 
-    static StringBuilder str = new StringBuilder();
-    static int current;
+    static Stack<Character> leftStack = new Stack<>();
+    static Stack<Character> rightStack = new Stack<>();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        str.append(br.readLine());
-        current = str.length();
+        String s = br.readLine();
+
+        for (int idx = 0; idx < s.length(); idx++) {
+            leftStack.push(s.charAt(idx));
+        }
+
         commandCnt = Integer.parseInt(br.readLine());
 
         for (int idx = 0; idx < commandCnt; idx++) {
-            String s = br.readLine();
-            execute(s);
+            String cmd = br.readLine();
+            execute(cmd);
         }
-
-        System.out.println(str.toString());
-
+        print();
     }
 
     public static void execute(String command) {
         switch (command.charAt(0)) {
             case 'P': {
-                String[] strings = command.split(" ");
-                str.insert(current, strings[1]);
-                current++;
+                leftStack.push(command.charAt(2));
                 break;
             }
             case 'L' : {
-                if (current > 0) current--;
+                if (leftStack.isEmpty()) break;
+                rightStack.push(leftStack.pop());
                 break;
             }
             case 'D': {
-                if (current < str.length()) current++;
+                if (rightStack.isEmpty()) break;
+                leftStack.push(rightStack.pop());
                 break;
             }
             case 'B': {
-                if (current == 0) break;
-
-                str.delete(current - 1, current);
-                execute("L");
-                break;
+                if (leftStack.isEmpty()) break;
+                leftStack.pop();
             }
-            default:
         }
     }
+
+    public static void print() {
+
+        StringBuilder str = new StringBuilder();
+
+        while (!leftStack.isEmpty()) {
+            rightStack.push(leftStack.pop());
+        }
+
+        while (!rightStack.isEmpty()) {
+            str.append(rightStack.pop());
+        }
+
+        System.out.println(str);
+    }
 }
+
+
